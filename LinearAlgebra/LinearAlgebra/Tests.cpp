c29ebaba609f89c8e74a1e26058d47d89ab7ca33
idoostrov <idoostrov@mail.tau.ac.il>
2019-05-16 14:32:36 +0300

    created basic test

---
+++ LinearAlgebra/LinearAlgebra/Tests.cpp
@@ -0,0 +1,28 @@
+#include "Matrix.h"
+#include "LLL.h"
+
+
+int main()
+{
+    Matrix A(2, 3);
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 3; j++) {
+            A[i][j] = 2 * (i + 1) * (i + 1) + i * j + 3 * j;
+        }
+    }
+
+    //cout << A << endl;
+
+    Matrix B(3, 2);
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 2; j++) {
+            B[i][j] = 7*(i + 1) * (i + 1) - 5 * (i + 3) * (j + 1) + 8 * j;
+        }
+    }
+    //cout << B ;
+
+    Matrix m = (A * B);
+
+    return 0;
+}
+
