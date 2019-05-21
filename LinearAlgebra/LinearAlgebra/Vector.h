2f7917a60bbadf0957dfcaba8c48d231ef8c13d9
Talkop <talthebear@gmail.com>
2019-05-22 00:18:52 +0300

    made template for vector

--- LinearAlgebra/LinearAlgebra/Vector.h
+++ LinearAlgebra/LinearAlgebra/Vector.h
@@ -7,27 +7,135 @@
 #define type int
 using namespace std;
 
+template <class T>
 class Array_Vector
 {
 private:
 	int length;
-	vector<type> elements;
-	mutable type scalar;
-	mutable type norm_squared;
+	vector<T> elements;
+	mutable T scalar;
+	mutable T norm_squared;
 public:
-	Array_Vector(const int length);
-	Array_Vector(const vector<type>& elements);
-	Array_Vector(const Array_Vector& other);
+	Array_Vector<T>(const int length)
+	{
+		this->norm_squared = -1;//Unintialized
+		this->length = length;
+		this->scalar = 1;
+		this->elements = vector<T>(length, 0);
+	}
+	Array_Vector<T>(const vector<T>& elements)
+	{
+		this->norm_squared = -1;//Unintialized
+		this->scalar = 1;
+		this->length = elements.size();
+		this->elements = vector<T>(elements);
+	}
+	Array_Vector<T>(const Array_Vector& other)
+	{
+		this->norm_squared = other.norm_squared;
+		this->scalar = other.scalar;
+		this->length = other.length;
+		this->elements = vector<type>(other.elements);
+	}
 
-	int len() const;
-	type operator[] (int index) const;
-	type& operator[](int index);
-	Array_Vector operator+ (const Array_Vector& other);
-	Array_Vector operator- (const Array_Vector& other);
-	type operator* (const Array_Vector& other);
-	Array_Vector operator*(const type scalar);
-	int norm();
+	int len() const
+	{
+		return this->length;
+	}
+	T operator[](const int index) const
+	{
+		return this->elements[index];
+	}
+	T& operator[](int index)
+	{
+		this->norm_squared = -1;
+		if (this->scalar != 1)
+		{
+			for (int i = 0; i < length; i++)
+			{
+				this->elements[i] *= this->scalar;
+			}
+			this->scalar = 1;
+		}
+		return this->elements[index];
+	}
+	Array_Vector<T> operator+(const Array_Vector<T>& other)
+	{
+		if (this->length != other.length)
+		{
+			throw new exception;
+		}
+		Array_Vector<T> vec(this->length);
+		for (int i = 0; i < this->length; i++)
+		{
+			vec.elements[i] = this->elements[i] * this->scalar + other.elements[i] * other.scalar;
+		}
+		return vec;
+	}
+	Array_Vector<T> operator-(const Array_Vector<T>& other)
+	{
+		if (this->length != other.length)
+		{
+			throw new exception;
+		}
+		Array_Vector<T> vec(this->length);
+		for (int i = 0; i < this->length; i++)
+		{
+			vec.elements[i] = this->elements[i] * this->scalar - other.elements[i] * other.scalar;
+		}
+		return vec;
+	}
+	T operator*(const Array_Vector<T>& other)
+	{
+		int tmp;
+		if (this->length != other.length)
+		{
+			cout << "What?";
+			cin >> tmp;
+			throw new exception;
+		}
+		int sum = 0;
+		for (int i = 0; i < this->length; i++)
+		{
+			sum += this->elements[i] * other.elements[i];
+		}
+		return sum * this->scalar*other.scalar;
+	}
+	Array_Vector<T> operator*(const T scalar)
+	{
+		Array_Vector<T> vec(this->elements);
+		vec.scalar = this->scalar*scalar;
+		return vec;
+	}
+	T norm()
+	{
+		if (this->norm_squared >= 0)
+		{
+			return this->norm_squared * this->scalar*this->scalar;
+		}
+		this->norm_squared = 0;
+		for (int i = 0; i < length; i++)
+		{
+			this->norm_squared += this->elements[i] * this->elements[i];
+		}
+		return this->norm_squared * this->scalar*this->scalar;
+	}
+	ostream& operator<<(ostream& os)
+	{
+		os << "[";
+		int length = this.len();
+		if (length != 0)
+		{
+			os << this[0];
+		}
+		for (int i = 1; i < length; i++)
+		{
+			os << ", ";
+			os << this[i];
+		}
+		os << "]\n";
+		return os;
+	}
 };
-ostream& operator<<(ostream& os, const Array_Vector& p);
 
 #endif//VECTOR_H
