568fccea10978d8370ca3a7a94deb3981580dc80
Talkop <talthebear@gmail.com>
2019-07-24 19:47:52 +0300

    First test added

---
+++ LinearAlgebra/LinearAlgebra/SmartTests.cpp
@@ -0,0 +1,27 @@
+//
+// Created by talth on 24/07/2019.
+// Contains tests for the SmartVector class
+//
+
+#include "SmartVector.h"
+
+#define LENGTH 20
+using namespace std;
+
+void test0()
+{
+    Smart_Vector<int> A(LENGTH);
+    cout << "Initialize Vector:" << endl;
+    cout << A << endl;
+    for (int i = 0; i < LENGTH; ++i)
+    {
+        A[i] = 1;
+    }
+    cout << "Sum Vector:"<< endl;
+    cout << A << endl;
+}
+
+int main()
+{
+    test0();
+}
