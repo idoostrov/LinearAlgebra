7cd29e9cfc8700dc298cce713a0fd7a3d5112086
Talkop <talthebear@gmail.com>
2019-08-06 18:44:44 +0300

    Tried to fix cmake :poop:

--- LinearAlgebra/LinearAlgebra/cmake-build-debug/CMakeFiles/LinearAlgebra.dir/cmake_clean.cmake
+++ LinearAlgebra/LinearAlgebra/cmake-build-debug/CMakeFiles/LinearAlgebra.dir/cmake_clean.cmake
@@ -1,5 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LinearAlgebra.dir/Tests/Tests.cpp.o"
+  "LinearAlgebra.pdb"
+  "LinearAlgebra.exe"
+  "libLinearAlgebra.dll.a"
+)
 
 # Per-language clean rules from dependency scanning.
-foreach(lang )
+foreach(lang CXX)
   include(CMakeFiles/LinearAlgebra.dir/cmake_clean_${lang}.cmake OPTIONAL)
 endforeach()
